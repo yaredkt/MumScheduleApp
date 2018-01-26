e2fa72197d967a520e591b8cafa8ac6c15eafb81
yaried tekie <yaredsonj@gmail.com>
2018-01-26 12:08:16 -0600

    mumshced is created

---
+++ MumSched/src/main/java/edu/mum/mumsched/MumSchedApplication.java
@@ -0,0 +1,12 @@
+package edu.mum.mumsched;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumSchedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MumSchedApplication.class, args);
+	}
+}
