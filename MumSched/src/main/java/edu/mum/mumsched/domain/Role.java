4e53a9b5612f3cefde8171a685a1b301a58f7b00
Mulugeta-Bisrat <34776292+Mulugeta-Bisrat@users.noreply.github.com>
2018-01-26 18:48:14 -0800

    commit for role domain class

--- MumSched/src/main/java/edu/mum/mumsched/domain/Role.java
+++ MumSched/src/main/java/edu/mum/mumsched/domain/Role.java
@@ -1,5 +1,40 @@
 package edu.mum.mumsched.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String role;
+	private String description;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
 
 }
